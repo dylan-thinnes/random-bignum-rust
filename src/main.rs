42a5a710bdf07bdd0e5b49b3c51e406550bb81d0
dylan-thinnes <dylan.thinnes@protonmail.com>
2019-02-24 11:28:25 +0000

    Handle insufficient arguments & invalid parses

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,16 @@ use std::env;
 fn main() {
     let args: Vec<_> = env::args().collect();
     let mut n: u32;
-    let n = args[1].trim().parse();
+    if args.len() > 1 {
+        let input = args[1].trim();
+        let parse_try = input.parse();
+        n = match parse_try {
+            Ok(num) => num,
+            Err(_)  => {
+                return;
+            },
+        };
+    } else {
+        n = rand::thread_rng().gen_range(100, 1000);
+    }
 }
