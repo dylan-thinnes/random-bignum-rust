a7ea68fd94f0e53e983c3bd4e93f481332356096
Dylan Thinnes <dylan.thinnes@protonmail.com>
2019-05-30 12:35:05 +0100

    Skip first arg (exe name) before parsing args

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use rand::Rng;
 use std::env;
 
 fn parse_input(args: Vec<String>) -> (u32, u64) {
-    let n: u32 = match args.get(1) {
+    let n: u32 = match args.get(0) {
         Some(s) => s.trim().parse()
                     .expect("Could not parse number of digits from {}.\
                              Please input a number!"),
@@ -14,7 +14,7 @@ fn parse_input(args: Vec<String>) -> (u32, u64) {
         }
     };
 
-    let base: u64 = match args.get(2) {
+    let base: u64 = match args.get(1) {
         Some(s) => s.trim().parse()
                     .expect("Could not parse a base from the second argument.\
                              Please input a number!"),
@@ -25,7 +25,7 @@ fn parse_input(args: Vec<String>) -> (u32, u64) {
 }
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
+    let args: Vec<_> = env::args().skip(1).collect();
     let (mut n, base) = parse_input(args);
 
     let mut r: u64;
