78a8dfdc4285b29a854ad10152cb980a6aff078f
Dylan Thinnes <dylan.thinnes@protonmail.com>
2019-05-30 12:27:28 +0100

    Move n & base definitions into match assignments

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,7 @@ use rand::Rng;
 use std::env;
 
 fn parse_input(args: Vec<String>) -> (u32, u64) {
-    let n: u32;
-    let base: u64;
-
-    n = match args.get(1) {
+    let n: u32 = match args.get(1) {
         Some(s) => s.trim().parse()
                     .expect("Could not parse number of digits from {}.\
                              Please input a number!"),
@@ -17,7 +14,7 @@ fn parse_input(args: Vec<String>) -> (u32, u64) {
         }
     };
 
-    base = match args.get(2) {
+    let base: u64 = match args.get(2) {
         Some(s) => s.trim().parse()
                     .expect("Could not parse a base from the second argument.\
                              Please input a number!"),
